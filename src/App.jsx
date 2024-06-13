8f98ad44d1877df34c4b22d0ce3fe834d9e12cc6
Alex Moeller <alex_moeller771@icloud.com>
2024-06-13 13:50:47 -0500

    netlify updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import AuthInputs from './components/AuthInputs.jsx';
-import Header from './components/Header.jsx';
+import AuthInputs from "./components/AuthInputs.jsx";
+import Header from "./components/Header.jsx";
 
 export default function App() {
   return (
@@ -8,6 +8,10 @@ export default function App() {
       <main>
         <AuthInputs />
       </main>
+      <div className="center">
+        <p>Note: The "Sign-In" and "Create Account" buttons are purely for aesthetics.</p>
+        <p>This page is purely for styling pratice, meaning the buttons do not have a function.</p>
+      </div>
     </>
   );
 }
